049ff5d7d834d0f0cfd95733a60a543123450385
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-19 19:42:29 +0200

    added about-intro and about-description components

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -6,9 +6,6 @@ export default function HomePage() {
     return (
         <div className={styles.intro} id='intro'>
             <div className={styles.wrapper}>
-                <div className={styles.presentation}>
-                    <p>Hi There, I'm a</p>
-                </div>
                 <div className={styles.title}>
                     <h3>FRENCH FRONTEND</h3>
                     <h3>WEB AND MOBILE</h3>
