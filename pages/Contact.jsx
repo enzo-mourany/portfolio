ee8261d7f7660754464bd072703ae2c483c068f7
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-23 11:31:08 +0200

    style contact page

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
+import styles from '../styles/contact.module.scss';
 
 export default function Contact() {
     return (
         <div className={styles.contact}>
             <div className={styles.header}>
                 <div className={styles.title}>
-                    <h2>MOURANY</h2>
+                    <h2>GET IN TOUCH</h2>
                 </div>
                 <div className={styles.message}>
-                    <h3>GET IN TOUCH</h3>
+                    <h3>WANT TO DISCUSS A NEW PROJECT?</h3>
                 </div>
                 <div className={styles.mailButton}>
-                    Send me an e-mail
+                    <p>Send me an e-mail</p>
                 </div>
             </div>
             <div className={styles.footer}>
                 <p>THANKS FOR YOUR VISIT!</p>
+                <div className={styles.links}>
+                    <ul>
+                        <li>
+                            <a href="#">GITHUB</a>
+                        </li>
+                        <li>
+                            <a href="#">LINKEDIN</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className={styles.links}>
-                <ul>
-                    <li>
-                        <a href="#">GITHUB</a>
-                    </li>
-                    <li>
-                        <a href="#">LINKEDIN</a>
-                    </li>
-                </ul>
-            </div>
+
 
 
         </div>
