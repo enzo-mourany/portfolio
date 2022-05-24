34b19932642ac2daccdada65c828e09b9ab9fdb6
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-24 08:47:35 +0200

    about description colored languages

--- components/about-description.jsx
+++ components/about-description.jsx
@@ -7,61 +7,68 @@ export default function AboutDescription() {
             <div className={styles.container}>
                 <div className={styles.text}>
                     <p>
-                        I started frontend development because I like when websites
-                        or applications are pleasant to use and intuitive. I love
-                        designs that are out of the ordinary. My experience in
-                        drawing could help me in this area.
+                        I started frontend development because I like when
+                        websites or applications are pleasant to use and
+                        intuitive. I love designs that are out of the ordinary. My
+                        experience in drawing could help me in this area.
                     </p>
                 </div>
                 <div className={styles.text}>
                     <p>
-                        I am working to become a fullstack JS developer as well as a
-                        blockchain developer. I would also like to be part of the
-                        Ethereum project
+                        I am working to become a fullstack JS developer as well as
+                        a blockchain developer. I would also like to be part of
+                        the Ethereum project
                     </p>
                 </div>
             </div>
 
             <div className={styles.container}>
                 <div className={styles.item}>
-                    <h3 className={styles.title}><span>01. </span>FRONTEND</h3>
+                    <h3 className={styles.title}>
+                        <span>01. </span>FRONTEND
+                    </h3>
                     <div className={styles.text2}>
-                        <p alt="frontend">
+                        <p alt='frontend'>
                             To become a frontend developer, I started by acquiring a
-                            solid foundation in HTML and CSS. Then I got into
-                            javascript. To start creating and working on projects, I
-                            learned React and React Native, then SASS which I liked much
-                            more than CSS. Finally I started to work a lot with NextJS
-                            to work on bigger projects. I also use some libraries like
-                            Three.JS or Parallax.JS.
+                            solid foundation in <span>HTML</span> and{' '}
+                            <span>CSS</span>. Then I got into javascript. To start
+                            creating and working on projects, I learned{' '}
+                            <span>React</span> and <span>React Native</span>, then{' '}
+                            <span>SASS</span> which I liked much more than CSS.
+                            Finally I started to work a lot with <span>NextJS </span>
+                            to work on bigger projects. I also use some libraries
+                            like
+                            <span> Three.JS</span> or <span>Parallax.JS</span>.
                         </p>
                     </div>
                 </div>
 
-
-
                 <div className={styles.item}>
-
-                    <h3 className={styles.title}><span>02. </span>BACKEND</h3>
+                    <h3 className={styles.title}>
+                        <span>02. </span>BACKEND
+                    </h3>
                     <div className={styles.text2}>
-                        <p alt="backend">
-                            I started backend development in my studies even though I
-                            already had some basics in python. I learned Java, SQL and
-                            C# with which I did some projects. To work on fullstack
-                            projects, I learned to use NodeJS, Express and MongoDB in
-                            particular to create REST APIs and to make online chat
-                            systems.
+                        <p alt='backend'>
+                            I started backend development in my studies even though
+                            I already had some basics in python. I learned <span>Java</span>, <span> SQL</span>
+                            and <span>C#</span> with which I did some projects. To work on
+                            fullstack projects, I learned to use <span>NodeJS</span>, <span>Express</span> and
+                            <span> MongoDB</span> in particular to create REST APIs and to make
+                            online chat systems.
                         </p>
                     </div>
                 </div>
 
-
                 <div className={styles.item}>
-
-                    <h3 className={styles.title}><span>03. </span>TEAM WORKING</h3>
+                    <h3 className={styles.title}>
+                        <span>03. </span>TEAM WORKING
+                    </h3>
                     <div className={styles.text2}>
-                        <p alt="team">
-                            I am used to working with a team and managing one. Thanks to the different projects I have carried out, I have learned to distribute the work between the members of a team and to create a pleasant group atmosphere.
+                        <p alt='team'>
+                            I am used to working with a team and managing one.
+                            Thanks to the different projects I have carried out, I
+                            have learned to distribute the work between the members
+                            of a team and to create a pleasant group atmosphere.
                         </p>
                     </div>
                 </div>
