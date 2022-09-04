73f1f9cede0de66264859d067c934fe05c3f24f4
Enzo Mourany <enzo.mourany@outlook.com>
2022-09-04 11:05:50 +0200

    responsive hamburger menu

--- pages/Topbar.jsx
+++ pages/Topbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import styles from '../styles/topbar.module.scss';
 
@@ -6,19 +6,20 @@ export default function Topbar({
     contactPageOpen,
     setContactPageOpen,
 }) {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={styles.topbar}>
-            <a className={styles.nav_logo} href='#'>Enzo Mourany</a>
-                <div className={styles.nav_items}>
-                    <a className={styles.item}>PROJECTS</a>
-                    <a className={styles.item}>ABOUT</a>
-                    <div className={styles.item} onClick={() => setContactPageOpen(!contactPageOpen)}>
-                        <a>CONTACT</a>
-                    </div>
+            <span className={styles.nav_logo} >Enzo Mourany</span>
+            <div className={styles.nav_items +  " "+ (isOpen && "open")}>
+                <a className={styles.item}>PROJECTS</a>
+                <a className={styles.item}>ABOUT</a>
+                <div className={styles.item} onClick={() => setContactPageOpen(!contactPageOpen)}>
+                    <a>CONTACT</a>
                 </div>
-                <div className={styles.nav_toggle}>
-                    <div className={styles.bar}></div>
-                </div>
+            </div>
+            <div className={styles.nav_toggle + " " + (isOpen && "open")} onClick={() => setIsOpen(!isOpen)}>
+                <div className={styles.bar}></div>
+            </div>
         </div>
     );
 }
