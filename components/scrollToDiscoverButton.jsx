263eb2af4f980a1ec1d4060e9ca22e415bbb055b
Enzo Mourany <enzo.mourany@outlook.com>
2022-09-24 19:45:45 +0200

    change topbar

--- components/scrollToDiscoverButton.jsx
+++ components/scrollToDiscoverButton.jsx
@@ -1,23 +1,23 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { HiArrowDown } from 'react-icons/hi';
 
 const scrollToDiscoverButton = () => {
-    const [showScrollToDiscoverButton, setShowScrollToDiscoverButton] = useState(true);
+  const [showScrollToDiscoverButton, setShowScrollToDiscoverButton] = useState(true);
 
-    useEffect(() => {
-        if(window.screenY > 300) {
-            setShowScrollToDiscoverButton(true);
-        } else {
-            setShowScrollToDiscoverButton(false);
-        }
-    }, [])
+  useEffect(() => {
+    if (window.screenY > 300) {
+      setShowScrollToDiscoverButton(true);
+    } else {
+      setShowScrollToDiscoverButton(false);
+    }
+  }, [])
 
-    const scrollToDiscover = () => {
-        window.scrollTo({
-            top: 200,
-            behavior: "smooth",
-        });
-    };
+  const scrollToDiscover = () => {
+    window.scrollTo({
+      top: 200,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div>
--- pages/Topbar.jsx
+++ pages/Topbar.jsx
@@ -11,15 +11,11 @@ export default function Topbar({
         <div className={styles.topbar}>
             <span className={styles.nav_logo} >Enzo Mourany</span>
             <div className={styles.nav_items + " " + (isOpen && "open")}>
-                <Link href="/Projects">Projects</Link>
-                <a className={styles.item}>PROJECTS</a>
-                <a className={styles.item}>ABOUT</a>
+                <Link className={styles.item} href="/Projects"><a className={styles.item}>Works</a></Link>
+                <a className={styles.item}>About</a>
                 <div className={styles.item} onClick={() => setContactPageOpen(!contactPageOpen)}>
-                    <a>CONTACT</a>
+                    <a>Contact</a>
                 </div>
-            </div>
-            <div className={styles.nav_toggle + " " + (isOpen && "open")} onClick={() => setIsOpen(!isOpen)}>
-                <div className={styles.bar}></div>
             </div>
         </div>
     );
