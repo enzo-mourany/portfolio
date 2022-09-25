0e2dce173a8a5931173dd1ecce92487e43b0c810
Enzo Mourany <enzo.mourany@outlook.com>
2022-09-25 19:18:44 +0200

    added new about page on homepage

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,14 +1,29 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import Link from 'next/link';
+import ProjectBox from '../components/project-box';
 import styles from '../styles/about.module.scss';
-import AboutIntro from '../components/about-intro';
-import AboutDescription from '../components/about-description';
 
-export default function AboutPage() {
+export default function About() {
     return (
-        <div className={styles.about}>
-            <AboutIntro />
-            <AboutDescription />
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.left}>
+                    <p>About me</p>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.main_description}>
+                        <p>I am passionate about web and mobile technology.</p>
+                        <p>I first started to learn frontend development by building e-commerce and storefront websites. I was able to learn a lot of technologies like React.JS, Next.Js and also more specific technologies like ParallaxJS or ThreeJS.</p>
+                        <p>In parallel, I started developing mobile applications in React Native, starting with an application of Tabata
+                        </p>
+                    </div>
+                    <div className={styles.description}>
+                        <p>After a few months, I decided to learn backend development to be able to create fullstack projects such as social networks. I chose javascript technologies with NodeJS, Express and MongoDB</p>
+                        <p>I like to make interfaces that are pleasant to use and intuitive for the users. Fluid and warm ashes.</p>
+                        <a href="#">More about me</a>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
--- pages/AboutPage.jsx
+++ pages/AboutPage.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import Link from 'next/link';
-import ProjectBox from '../components/project-box';
+import styles from '../styles/about-page.module.scss';
+import AboutIntro from '../components/about-intro';
+import AboutDescription from '../components/about-description';
 
 export default function AboutPage() {
     return (
-        <div>
-            About me
+        <div className={styles.about}>
+            <AboutIntro />
+            <AboutDescription />
         </div>
-    );
+    )
 }
