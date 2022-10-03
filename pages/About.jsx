8697ad72fd7a59809d8beb2c705961ff0d1ebb70
Enzo Mourany <enzo.mourany@outlook.com>
2022-10-03 11:21:54 +0200

    better about page

--- pages/About.jsx
+++ pages/About.jsx
@@ -8,12 +8,14 @@ export default function About() {
         <div className={styles.container}>
             <div className={styles.wrapper}>
                 <div className={styles.left}>
-                    <p>About me</p>
+                    <p>01/</p>
                 </div>
                 <div className={styles.right}>
                     <div className={styles.main_description}>
                         <p>I am passionate about web and mobile technology.</p>
+                        <br />
                         <p>I first started to learn frontend development by building e-commerce and storefront websites. I was able to learn a lot of technologies like React.JS, Next.Js and also more specific technologies like ParallaxJS or ThreeJS.</p>
+                        <br />
                         <p>In parallel, I started developing mobile applications in React Native, starting with an application of Tabata
                         </p>
                     </div>
