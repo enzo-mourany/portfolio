6e37e7d8d21137ccc90afa70467e4514d3bc59e6
Enzo Mourany <enzo.mourany@outlook.com>
2022-10-03 13:18:31 +0200

    better topbar

--- pages/Topbar.jsx
+++ pages/Topbar.jsx
@@ -6,17 +6,32 @@ export default function Topbar({
     contactPageOpen,
     setContactPageOpen,
 }) {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
+    const [isHamburgerMenuShown, setIsHamburgerMenuShown] = useState(true);
     return (
         <div className={styles.topbar}>
             <span className={styles.nav_logo} >Enzo Mourany</span>
-            <div className={styles.nav_items + " " + (isOpen && "open")}>
-                <Link className={styles.item} href="/Projects"><a className={styles.item}>Works</a></Link>
-                <a className={styles.item}>About</a>
-                <div className={styles.item} onClick={() => setContactPageOpen(!contactPageOpen)}>
-                    <a>Contact</a>
+            {isHamburgerMenuShown ? (
+                <div className={styles.nav_items + " " + (isOpen && "open")}>
+                    <Link className={styles.item} href="/Projects"><a className={styles.item}>Works</a></Link>
+                    <a className={styles.item}>About</a>
+                    <div className={styles.item} onClick={() => setContactPageOpen(!contactPageOpen)}>
+                        <a>Contact</a>
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className={styles.nav_items}>
+                    <Link className={styles.item} href="/Projects"><a className={styles.item}>Works</a></Link>
+                    <a className={styles.item}>About</a>
+                    <div className={styles.item} onClick={() => setContactPageOpen(!contactPageOpen)}>
+                        <a>Contact</a>
+                    </div>
+                </div>
+            )
+            }
+
+
+
         </div>
     );
 }
