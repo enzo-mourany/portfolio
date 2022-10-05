0d49464842c8189efee1bc04ef353d2f141be0ab
Enzo Mourany <enzo.mourany@outlook.com>
2022-10-05 08:23:35 +0200

    added reveal animation topbar

--- pages/Topbar.jsx
+++ pages/Topbar.jsx
@@ -10,8 +10,27 @@ export default function Topbar({
 
     const [isOpen, setIsOpen] = useState(false);
 
+
+    const [show, setShow] = useState(true);
+    let lastScrollTop = 0;
+    const controlTopbar = () => {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            setShow(false);
+        } else {
+            setShow(true);
+        }
+        lastScrollTop = scrollTop;
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlTopbar);
+        return () => window.removeEventListener('scroll', controlTopbar);
+    }, []);
+
+
     return (
-        <div className={styles.topbar}>
+        <div className={show ? styles.topbar : styles.topbar_hidden}>
             <Link href={"/"}>
                 <a className={styles.nav_logo}>Enzo Mourany</a>
             </Link>
