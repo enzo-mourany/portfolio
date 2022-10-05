b70d5867687e5473d32d0b214647a214867791ac
Enzo Mourany <enzo.mourany@outlook.com>
2022-10-05 12:51:24 +0200

    change project page

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import ProjectCard from '../components/project-card';
 import Topbar from './Topbar';
+import ProjectBox from '../components/project-box';
+import styles from '../styles/projects.module.scss';
 
 
 const defaultEndpoint = 'https://raw.githubusercontent.com/enzo-mourany/projects-api/gh-pages/projects.json';
@@ -21,17 +22,29 @@ export default function Projects({ data }) {
     return (
         <>
             <Topbar />
-            <div>
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <div className={styles.left}>
+                        <p>01/</p>
+                    </div>
+                    <div className={styles.right}>
+                        <h1>Projects</h1>
+                        <p>06</p>
+                    </div>
+                </div>
                 <div>
                     {data.map((result) => (
                         // eslint-disable-next-line react/jsx-key
-                        <ProjectCard
+                        <ProjectBox
+                            //className={styles.projectBox}
                             id={result.id}
                             title={result.title}
                             description={result.description}
                             githublink={result.githublink}
                             tech={result.tech}
+                            type={result.type}
                             date={result.date}
+                            img={result.img}
                         />
                     ))}
                 </div>
