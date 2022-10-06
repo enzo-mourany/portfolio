c9d4382ac8c2f0f763f973b63378bee37d3f181e
Enzo Mourany <enzo.mourany@outlook.com>
2022-10-06 16:58:10 +0200

    added contact animation

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -12,8 +12,36 @@ export default function Contact() {
           <div className={styles.mailButton}>
             <p>Send me an message</p>
             <a href='mailto:enzo.mourany@outlook.com'>
-              enzo.mourany@outlook.com
+              <ul>
+                <li>
+                  <span>E</span>
+                  <span>N</span>
+                  <span>Z</span>
+                  <span>O</span>
+                  <span>.</span>
+                  <span>M</span>
+                  <span>O</span>
+                  <span>U</span>
+                  <span>R</span>
+                  <span>A</span>
+                  <span>N</span>
+                  <span>Y</span>
+                  <span>@</span>
+                  <span>O</span>
+                  <span>U</span>
+                  <span>T</span>
+                  <span>L</span>
+                  <span>O</span>
+                  <span>O</span>
+                  <span>K</span>
+                  <span>.</span>
+                  <span>C</span>
+                  <span>O</span>
+                  <span>M</span>
+                </li>
+              </ul>
             </a>
+
           </div>
         </div>
         <div className={styles.footer}>
