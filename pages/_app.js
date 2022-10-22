20c32404bd22c060433e2d0267357d11aed3fea0
Enzo Mourany <enzo.mourany@outlook.com>
2022-10-22 11:02:53 +0200

    updata animation delay

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,20 @@ import '../styles/globals.scss'
 import { motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps, router }) {
-  return (
-          <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
-              pageInitial: {
-                  opacity: 0,
-              },
-              pageAnimate: {
-                  opacity: 1,
-              }
-          }}>
-      <Component {...pageProps} />
-          </motion.div>
-  )
+    return (
+        <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
+            pageInitial: {
+                opacity: 0,
+                duration: 1
+            },
+            pageAnimate: {
+                opacity: 1,
+                duration: 1
+            }
+        }}>
+            <Component {...pageProps} />
+        </motion.div>
+    )
 }
 
 export default MyApp
